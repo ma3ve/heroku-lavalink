6a95d17704cb0b71040ff4561ef565ee5081097b
Emkay <43314821+ma3ve@users.noreply.github.com>
2021-09-20 13:06:22 +0000

    changes

--- bootstrap.js
+++ bootstrap.js
@@ -6,6 +6,7 @@ let application = fs.readFileSync("./application.yml", "utf8")
 
 if (process.env.PORT) {
     application = application.replace("DYNAMICPORT", process.env.PORT)
+    console.log("PORT.........................................................", PORT);
 }
 
 if (process.env.PASS) {
@@ -15,20 +16,20 @@ fs.writeFileSync("./application.yml", application)
 
 const download = function (url, dest, cb) {
     const file = fs.createWriteStream(dest);
-    fetch(url).then(res=>{
+    fetch(url).then(res => {
         res.body.pipe(file)
         console.log("Downloading Lavalink.jar")
         file.on("finish", function () {
             console.log("Downloaded Lavalink.jar")
             file.close(cb);
         });
-        file.on("error", function(err){
-            console.error("Filestream error while downloading Lavalink: "+err)
+        file.on("error", function (err) {
+            console.error("Filestream error while downloading Lavalink: " + err)
         })
     })
-    .catch(function(err){
-        console.error("Fetch error while downloading Lavalink: "+err)
-    })
+        .catch(function (err) {
+            console.error("Fetch error while downloading Lavalink: " + err)
+        })
 };
 
 function startLavalink() {
@@ -60,26 +61,26 @@ console.log("Fetching latest Lavalink.jar url...")
 fetch("https://api.github.com/repos/freyacodes/Lavalink/releases/latest")
     .then(res => res.json())
     .then(json => {
-        if(json.assets[0] && json.assets[0].browser_download_url){
-            console.log("Found: "+json.assets[0].browser_download_url)
+        if (json.assets[0] && json.assets[0].browser_download_url) {
+            console.log("Found: " + json.assets[0].browser_download_url)
             download(json.assets[0].browser_download_url, "./Lavalink.jar", startLavalink)
-        }else{
+        } else {
             console.warn("Could not find .jar for latest release!")
             console.warn("Attempting to download previous release...")
-            
+
             let priorVersion = json["tag_name"].split(".")
-            priorVersion[priorVersion.length-1] = Number(priorVersion[priorVersion.length-1])-1
-            priorVersion[0] = priorVersion[0].replace("v","")
+            priorVersion[priorVersion.length - 1] = Number(priorVersion[priorVersion.length - 1]) - 1
+            priorVersion[0] = priorVersion[0].replace("v", "")
             priorVersion = priorVersion.join(".")
 
             let priorDL_URL = `https://github.com/freyacodes/Lavalink/releases/download/${priorVersion}/Lavalink.jar`
-            console.log("Found: "+priorDL_URL)
+            console.log("Found: " + priorDL_URL)
             download(priorDL_URL, "./Lavalink.jar", startLavalink)
         }
 
     })
-    .catch(err =>{
-        console.error("Error occured when fetching latest release url: "+err)
+    .catch(err => {
+        console.error("Error occured when fetching latest release url: " + err)
     });
 
 
